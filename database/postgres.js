b8d5a84ef5cbd849e78023d60175f4ae3b45bc64
Patrick S.K <shemap300@gmail.com>
2023-05-07 01:12:33 +0200

    config pg

--- database/postgres.js
+++ database/postgres.js
@@ -1,7 +1,6 @@
 const { Client } = require("pg");
 
-const PG_CONN_STRING =
-  "postgres://qupiheiu:iFBdevRgxC26lT3ONhqkA5Ryt8MD4f9x@arjuna.db.elephantsql.com/qupiheiu";
+const PG_CONN_STRING = process.env.DATABASE_URL;
 
 const pgClient = new Client({
   connectionString: PG_CONN_STRING,
