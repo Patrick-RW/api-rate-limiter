f617a5735110dd6309e275ebf2d56c40e246ec69
Patrick S.K <shemap300@gmail.com>
2023-05-07 01:27:28 +0200

    config pg

--- database/postgres.js
+++ database/postgres.js
@@ -4,6 +4,9 @@ const PG_CONN_STRING = process.env.DATABASE_URL;
 
 const pgClient = new Client({
   connectionString: PG_CONN_STRING,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pgClient.connect().catch(console.error);
